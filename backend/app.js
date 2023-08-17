d5623b4c8afed6efd782d7955ecfc7d3bb40e2f0
raph5 <raphguyader@gmail.com>
2023-08-17 16:08:07 +0200

    sso fix

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,24 @@
 const SingleSignOn = require('./sso')
 const express = require('express')
 
-console.log(SingleSignOn)
+// acces token parser
+function parseAccesToken(info) {
+  if(
+    !info.access_token ||
+    !info.refresh_token ||
+    !info.decoded_access_token.sub ||
+    !info.decoded_access_token.name ||
+    info.token_type !== 'Bearer'
+  ) throw new Error("invalid token data")
+
+  return {
+    access_token: info.access_token,
+    refresh_token: info.refresh_token,
+    expiration: info.decoded_access_token.exp,
+    character_id: parseInt(info.decoded_access_token.sub.split(':')[2]),
+    character_name: info.decoded_access_token.name
+  }
+}
 
 // dotenv setup
 require('dotenv').config()
@@ -13,6 +30,7 @@ if(!process.env.SSO_CLIENT_ID || !process.env.SSO_SECRET_KEY) {
 const PORT = 8000
 const CALLBACK_URI = `http://localhost:${PORT}/sso`
 const SCOPE = 'esi-planets.manage_planets.v1'
+
 // SSO setup
 const sso = new SingleSignOn(
   process.env.SSO_CLIENT_ID,
@@ -30,12 +48,32 @@ app = express()
 
 // Handle the SSO callback
 app.get('/sso', async (req, res) => {
+  try {
+    const code = req.query.code
+    const info = await sso.getAccessToken(code)
+    const token_data = parseAccesToken(info)
+    
+    res.cookie('token_data', JSON.stringify(token_data))
+    res.redirect(302, '/app');
+  }
+  catch {
+    res.redirect(302, '/?loginerror');
+  }
+})
 
-  const code = req.query.code
-  const info = await sso.getAccessToken(code)
 
-  res.send(info.decoded_access_token)
+// Handle SSO token refresh
+app.get('/api/refreshtoken', async (req, res) => {
+  try {
+    const token = req.query.token
+    const info = await sso.getAccessToken(token, true)
+    const token_data = parseAccesToken(info)
 
+    res.send(token_data)
+  }
+  catch (e) {
+    res.send({ 'error': 'error' })
+  }
 })
 
 
